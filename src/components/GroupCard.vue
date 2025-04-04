a9db8bc525180f0a22eecc9975e5f2ad7606d0e9
c0057166 <b.d.hall2@newcastle.ac.uk>
2025-04-04 16:55:15 +0100

    Image fix

--- src/components/GroupCard.vue
+++ src/components/GroupCard.vue
@@ -4,7 +4,7 @@
     class="bg-white shadow-md rounded-xl p-4 flex justify-between items-center transition hover:shadow-lg"
   >
     <div class="justify-between flex items-center">
-      <img :src="group.icon_url ?? '/src/assets/images/green-football-md.png'" class="w-10 h-10 mr-3" alt="Group Logo"/>
+      <img :src="group.icon_url ?? '/images/green-football-md.png'" class="w-10 h-10 mr-3" alt="Group Logo"/>
       <div>
         <h3 class="text-lg font-semibold">{{ group.name }}</h3>
         <p class="text-gray-500">{{ group.member_count }} members</p>
